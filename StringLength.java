5d2548660cca865c7c4712c454abcd30e2c0a088
mohithgowda187 <mohithgowdahk06@gmail.com>
2026-01-30 09:06:29 +0530

    Add StringLength class to calculate string length

---
+++ StringLength.java
@@ -0,0 +1,8 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String text = "Hello World"; 
+        int length = text.length();
+        System.out.println("The string is: \"" + text + "\"");
+        System.out.println("Length of the string = " + length);
+    }
+}
